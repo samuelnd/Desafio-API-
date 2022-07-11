eb22815a1e4d52b2998d658a8c9da05102de096e
samuel <samuelsnds09@outlook.com>
2022-07-11 14:19:50 -0300

    created file AppError

--- src/server.js
+++ src/server.js
@@ -4,11 +4,27 @@ const database = require("./database/sqlite");
 
 const routes = require('./routes');
 
+const AppError = require("./utils/AppError");
+
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 database();
-app.use(routes);
+
+app.use((error, request, response, next) => {
+    if(error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "Error",
+            message: error.message
+        });
+    }
+
+    return response.status(500).json({
+        error: "Error",
+        message: "Internal Server Error"
+    });
+});
 
 const PORT = 3333;
 
