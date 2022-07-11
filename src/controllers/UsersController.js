fdb759f28fb0b416f54c7652fb82e42b36aea007
samuel <samuelsnds09@outlook.com>
2022-07-11 14:00:57 -0300

    created folder controller and install knex

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,10 +1,15 @@
 const {hash, compare} = require("bcryptjs");
 
+const sqliteConnection = require("../database/sqlite");
+
 class UsersController {
 async create(request, response) {
     const {name, email, password} = request.body;
 
     const hashedPassword = await hash(password, 8);
+    const database = await sqliteConnection();
+
+    database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password]);
 
     response.status(201).json({name, email, hashedPassword});
 }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const database = require("./database/sqlite");
+
 const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
 
+database();
 app.use(routes);
 
 const PORT = 3333;
