2a3bab0b765c88524110fdc26f6a696c09536905
samuel <samuelsnds09@outlook.com>
2022-07-13 16:00:49 -0300

    created migration MoviesNotes

--- src/database/knex/migrations/20220713181637_createMovieNotes.js
+++ src/database/knex/migrations/20220713181637_createMovieNotes.js
@@ -1,15 +1,13 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("movies", table => {
+    table.increments("id"),
+    table.text("title"),
+    table.text("description"),
+    table.integer("rating"),
+    table.integer("user_id").references("id").inTable("users"),
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+    table.timestamp("created_at").default(knex.fn.now()),
+    table.timestamp("updated_at").default(knex.fn.now())
+    });
+    
+    
+    exports.down = knex => knex.schema.dropTable("movies");
